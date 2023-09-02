9895924b8284977d1bf6246103331dd77fd3b477
jwlashley <jwlashley@atmo.space>
2023-09-01 23:43:21 -0500

    Created function to reverse string and return it.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    phrase.split("");
+    let reversedPhrase = "";
+    let characterCount = phrase.length;
+    for(let i = characterCount - 1; i >= 0; i--){
+        reversedPhrase += phrase[i];
+    }
+    return reversedPhrase;
 };
 
 // Do not edit below this line
